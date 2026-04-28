cd70e8b68ee17e429bc6b4f7c17fc40b5de419d8
alvi103-png <alvi103@gmail.com>
2026-04-28 19:26:08 +0200

    add: JS done.

---
+++ js/contactForm.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("formContacto");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const mensajeUsuario = document.getElementById("mensaje").value;
+    
+
+    console.log({
+        nombre: nombre,
+        email: email,
+        mensaje: mensajeUsuario,
+
+    })
+
+    const mensajeUI = document.getElementById("formMensaje");
+
+    mensajeUI.textContent = "Mensaje enviado correctamente!";
+    mensajeUI.classList.add("exito");
+
+
+    form.reset();
+
+
+});
